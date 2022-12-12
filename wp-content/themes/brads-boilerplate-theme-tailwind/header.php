b800d523b85f56801dc7f36b500d6115591e788e
Alban de Braquilanges <alban2bqs@gmail.com>
2022-12-12 11:19:18 +0100

    correction menu

--- wp-content/themes/brads-boilerplate-theme-tailwind/header.php
+++ wp-content/themes/brads-boilerplate-theme-tailwind/header.php
@@ -7,8 +7,26 @@
   <?php wp_head(); ?>
 </head>
 
-<body <?php body_class();
-      wp_nav_menu();
-      echo ""
+<body class='bg-red-500' <?php body_class(); ?>>
+  <?php
+  // wp_nav_menu();
+  $footerMenu = wp_get_nav_menu_items(
+    get_nav_menu_locations('footer-social')['footer-social']
+  );
+  //dump($footerMenu);
 
-      ?>
+
+  ?>
+  <nav>
+    <ul>
+      <?php foreach ($footerMenu as $item) : ?>
+
+        <li>
+          <a href="<?= $item->url ?>">
+            <?= $item->title ?>
+          </a>
+        </li>
+
+      <?php endforeach; ?>
+    </ul>
+  </nav>
---
+++ wp-content/themes/brads-boilerplate-theme-tailwind/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '11b75e8dd5af5e123593f7874366c44bdc8e6e38',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '11b75e8dd5af5e123593f7874366c44bdc8e6e38',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-mbstring' => array(
+            'pretty_version' => 'v1.27.0',
+            'version' => '1.27.0.0',
+            'reference' => '8ad114f6b39e2c98a8b0e3bd907732c207c2b534',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'symfony/var-dumper' => array(
+            'pretty_version' => 'v6.2.1',
+            'version' => '6.2.1.0',
+            'reference' => '1e7544c8698627b908657e5276854d52ab70087a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/var-dumper',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
