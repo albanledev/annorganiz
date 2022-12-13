8b57fd7b3df2fa34138a34fe5ca653419f53ce43
BARDAT Ryan <105910237+REIG3N@users.noreply.github.com>
2022-12-13 11:32:56 +0100

    footer v2

--- wp-content/themes/brads-boilerplate-theme-tailwind/footer.php
+++ wp-content/themes/brads-boilerplate-theme-tailwind/footer.php
@@ -7,15 +7,14 @@ $footerMenu = wp_get_nav_menu_items(
 );
 ?>
 
-<nav class="flex justify-between px-20 py-20 border-solid border-2">
-<img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="logo">
-    <div class="uppercase text-center">
+<nav class="flex flex-wrap max-lg:flex-col justify-between px-20 py-20 gap-12 max-lg:gap-8  max-lg:px-10 ">
+    <div class="uppercase ">
         <p>Annorganiz@gmail.com</p>
         <p>Téléphone: 0612132432</p>
         <p>Adresse rue fictive</p>
         <p>Paris 5e</p>
     </div>
-    <div class="flex justify-evenly items-center gap-12">
+    <div class="flex justify-evenly items-center gap-12 max-lg:gap-8 ">
         <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-facebook.png" alt="logo instagram" class="w-9 h-9">
         <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-instagram.png" alt="logo linkedin" class="w-9 h-9">
         <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-linkedin.png" alt="logo facebook" class="w-9 h-9">
