6b46b53a100f60e2a7f6b8ec22700b906821cfa9
BARDAT Ryan <105910237+REIG3N@users.noreply.github.com>
2022-12-14 15:51:26 +0100

    footer v3 en cours

--- wp-content/themes/brads-boilerplate-theme-tailwind/footer.php
+++ wp-content/themes/brads-boilerplate-theme-tailwind/footer.php
@@ -4,29 +4,51 @@ $footerMenu = wp_get_nav_menu_items(
     get_nav_menu_locations('footer-social')['footer-social']
 );
 ?>
+<nav class="">
+    <div class="flex flex-wrap max-lg:flex-col justify-between px-20 py-20 gap-12 max-lg:gap-8  max-lg:px-10 ">
+        <div class="uppercase font-['Poppins'] font-bold text-footer-purple">
+            <p class="tracking-wide text-purple-900">Annorganiz@gmail.com</p>
+            <p class="tracking-wide text-purple-900">Téléphone: 0612132432</>
+            <p class="tracking-wide text-purple-900">Adresse rue fictive</>
+            <p class="tracking-wide text-purple-900">Paris 5e</>
+        </div>
+        <div class="flex justify-evenly items-center gap-12 max-lg:gap-8 ">
+            <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-facebook.png" alt="logo instagram" class="w-9 h-9">
+            <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-instagram.png" alt="logo linkedin" class="w-9 h-9">
+            <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-linkedin.png" alt="logo facebook" class="w-9 h-9">
+        </div>
 
-<<<<<<< HEAD
-<nav class="flex flex-wrap max-lg:flex-col justify-between px-20 py-20 gap-12 max-lg:gap-8  max-lg:px-10 ">
-    <div class="uppercase ">
-=======
-<nav class="flex justify-between px-20 py-20 border-solid border-2">
-    <!-- <img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="logo"> -->
-    <div class="uppercase text-center">
->>>>>>> 81f040d6d6dde2969e8ac85feffcbf8771e10e50
-        <p>Annorganiz@gmail.com</p>
-        <p>Téléphone: 0612132432</p>
-        <p>Adresse rue fictive</p>
-        <p>Paris 5e</p>
-    </div>
-    <div class="flex justify-evenly items-center gap-12 max-lg:gap-8 ">
-        <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-facebook.png" alt="logo instagram" class="w-9 h-9">
-        <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-instagram.png" alt="logo linkedin" class="w-9 h-9">
-        <img src="<?php echo get_template_directory_uri() ?>/img/footer-logo-linkedin.png" alt="logo facebook" class="w-9 h-9">
     </div>
 </nav>
+<div class="flex text-center bg-red-400">
+
+</div>
+
+<div class="flex text-center text-xs" >
+    <div class="flex text-center text-xs ">
+        <p> COPYRIGHT 2019 2022 - ANNORGANIZ </p>
+        <p> | </p>
+        <a href=""> ACCUEIL </a>
+        <p> | </p>
+        <a href=""> CONTACT </a>
+        <p> | </p>
+        <a href=""> A PROPOS </a>
+        <p> | </p>
+        <a href=""> PRESTATION </a>
+        <p> | </p>
+        <a href=""> MENTION LEGALE </a>
+        <p> | </p>
+        <a href=""> BLOG </a>
+    </div>
+
+</div>
+
+
+
+
+
+
 
 <?php wp_footer(); ?>
-
 </body>
-
 </html>
--- wp-content/themes/brads-boilerplate-theme-tailwind/tailwind.config.js
+++ wp-content/themes/brads-boilerplate-theme-tailwind/tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   content: ["./**/*.php", "./src/**/*.js"],
+  theme: {
+
+      colors:{
+        "footer-purple": "#3B0075",}
+
+  },
   plugins: [require("@tailwindcss/typography")]
 }
